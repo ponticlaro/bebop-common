08e940496edfb181128fae6ec7a94f37451a4b1a
Cristiano Baptista <cristianobaptista@designdesu.com>
2016-05-13 11:05:10 +0200

    added Markdown and Smartypants utilities

--- src/Utils.php
+++ src/Utils.php
@@ -4,15 +4,32 @@ namespace Ponticlaro\Bebop\Common;
 
 class Utils
 {
-  private function __construct(){}
+  /**
+   * Making sure class cannot get instantiated
+   */
+  private function __construct() {}
 
-  private function __clone(){}
+  /**
+   * Making sure class cannot get instantiated
+   */
+  private function __clone() {}
 
+  /**
+   * Checks if wordpress installation is a network
+   * 
+   * @return boolean True if it is, false otherwise
+   */
   public static function isNetwork()
   {
     return is_multisite();
   }
 
+  /**
+   * Converts string from camelcase to underscore pattern
+   * 
+   * @param  string $string String to modify
+   * @return string         String converted to underscore pattern
+   */
   public static function camelcaseToUnderscore($string) 
   {
     preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $string, $matches);
@@ -23,7 +40,17 @@ class Utils
     return implode('_', $ret);
   }
 
-  public static function slugify($string, $options = array("separator" => "_") )
+  /**
+   * Slugifies string:
+   * - Removes accents
+   * - Converts everything to lowercase
+   * - Replaces white spaces with a separator; default separator is underscore
+   * 
+   * @param  string $string  String to modify
+   * @param  array  $options Slugification options
+   * @return string          Slugified string
+   */
+  public static function slugify($string, $options = array("separator" => "_"))
   { 
     // Remove any accented characters
     $string = \remove_accents($string);
@@ -35,6 +62,56 @@ class Utils
     $string = \preg_replace("/ /", $options["separator"], $string);
 
     return $string;
+  }
+
+  /**
+   * Parses markdown and optionally fixes punctuation with SmartyPants
+   *
+   * @param  string $content String to be parsed
+   * @return string          Parsed string
+   */
+  public static function parseMarkdown($content, $options = ["fix_punctuation" => false])
+  {
+    if (!is_string($content)) 
+      return null;
+
+    // Strip whitepace from the start of all lines with HTML
+    // so that the Markdown parser does not wrap HTML within a <pre> element
+    $clean_content = '';
+
+    foreach (explode("\n", $content) as $line) {
+
+      if (trim($line) == '') {
+        $clean_content .= "\n";
+      }
+
+      else {
+        $clean_content .= preg_replace('/^(\\s+[?!<])/m', '<', $line) ."\n";
+      }
+    }
+
+    $content = $clean_content;
+    $parser  = new \ParsedownExtra();
+    $content = $parser->text($content);
+
+    if ($options['fix_punctuation'])
+      $content = static::fixPunctuation($content);
+
+    return $content;
+  }
+
+  /**
+   * Fixes punctuation with SmartyPants
+   *
+   * @param  string $content String to be parsed
+   * @return string          Parsed string
+   */
+  public static function fixPunctuation($content)
+  {
+    if (!is_string($content)) 
+      return null;
+
+    return \Michelf\SmartyPants::defaultTransform($content);
   }
 
   /**
@@ -70,7 +147,8 @@ class Utils
    */
   public static function getPathUrl($path, $relative = false)
   {
-    if (!is_string($path)) return null;
+    if (!is_string($path)) 
+      return null;
 
     $content_base = basename(WP_CONTENT_URL);
     $path         = str_replace(ABSPATH, '', $path);
@@ -86,53 +164,53 @@ class Utils
    * @param  array    $args     Callabler arguments
    * @return array              List of control elements names
    */
-    public static function getControlNamesFromCallable($callable, array $args = array())
-    {   
-      $names = array();
+  public static function getControlNamesFromCallable($callable, array $args = array())
+  {   
+    $names = array();
 
-      ob_start();
+    ob_start();
 
-      call_user_func_array($callable, $args);
-      
-      $html = ob_get_contents();
+    call_user_func_array($callable, $args);
+    
+    $html = ob_get_contents();
 
-      ob_end_clean();
+    ob_end_clean();
 
-      if (trim($html)) {
+    if (trim($html)) {
 
-          $doc = new \DOMDocument;
-          @$doc->loadHTML($html);
+      $doc = new \DOMDocument;
+      @$doc->loadHTML($html);
 
-          $patterns = array(
-            "/\[\]/",       // e.g. items[]
-            "/\[[^\]]+\]/", // e.g. parent[child]
-          );
+      $patterns = array(
+        "/\[\]/",       // e.g. items[]
+        "/\[[^\]]+\]/", // e.g. parent[child]
+      );
 
-          foreach ($doc->getElementsByTagname('input') as $el) {
+      foreach ($doc->getElementsByTagname('input') as $el) {
 
-              $name = $el->getAttribute('name');
+        $name = $el->getAttribute('name');
 
-              if ($name)
-                  $names[] = preg_replace($patterns, '', $name);
-          }
+        if ($name)
+          $names[] = preg_replace($patterns, '', $name);
+      }
 
-          foreach ($doc->getElementsByTagname('select') as $el) {
+      foreach ($doc->getElementsByTagname('select') as $el) {
 
-              $name = $el->getAttribute('name');
+        $name = $el->getAttribute('name');
 
-              if ($name)
-                  $names[] = preg_replace($patterns, '', $name);
-          }
+        if ($name)
+          $names[] = preg_replace($patterns, '', $name);
+      }
 
-          foreach ($doc->getElementsByTagname('textarea') as $el) {
+      foreach ($doc->getElementsByTagname('textarea') as $el) {
 
-              $name = $el->getAttribute('name');
+        $name = $el->getAttribute('name');
 
-              if ($name)
-                  $names[] = preg_replace($patterns, '', $name);
-          }
-        }
-
-        return array_unique($names);
+        if ($name)
+          $names[] = preg_replace($patterns, '', $name);
+      }
     }
+
+    return array_unique($names);
+  }
 }
