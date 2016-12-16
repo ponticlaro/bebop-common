7453cbd363e3643e3e30a10df34a2b3aaa70cdc2
Cristiano Baptista <cristianobaptista@designdesu.com>
2016-12-16 14:39:19 +0100

    added missing FactoryAbstract::get() method; cleaned class spacing

--- src/Patterns/FactoryAbstract.php
+++ src/Patterns/FactoryAbstract.php
@@ -4,103 +4,107 @@ namespace Ponticlaro\Bebop\Common\Patterns;
 
 abstract class FactoryAbstract {
 
-	/**
-	 * List of manufacturable classes
-	 * 
-	 * @var array
-	 */
-	protected static $manufacturable = array();
+  /**
+   * List of manufacturable classes
+   * 
+   * @var array
+   */
+  protected static $manufacturable = array();
 
-	/**
-	 * Making sure class cannot get instantiated
-	 */
-	protected function __construct() {}
+  /**
+   * Making sure class cannot get instantiated
+   */
+  protected function __construct() {}
 
-	/**
-	 * Making sure class cannot get instantiated
-	 */
-	protected function __clone() {}
+  /**
+   * Making sure class cannot get instantiated
+   */
+  protected function __clone() {}
 
-	/**
-	 * Adds a new manufacturable class
-	 * 
-	 * @param string $type  Object type ID
-	 * @param string $class Full namespace for a class
-	 */
-	public static function set($type, $class)
-	{
-		static::$manufacturable[$type] = $class;
-	}
+  /**
+   * Adds a new manufacturable class
+   * 
+   * @param string $type  Object type ID
+   * @param string $class Full namespace for a class
+   */
+  public static function set($type, $class)
+  {
+    static::$manufacturable[$type] = $class;
+  }
 
-	/**
-	 * Removes a new manufacturable class
-	 * 
-	 * @param string $type  Object type ID
-	 */
-	public static function remove($type)
-	{
-		if (isset(static::$manufacturable[$type])) unset(static::$manufacturable[$type]);
-	}
+  /**
+   * Returns the class for the target manufacturable
+   * 
+   * @param  string $type Object type ID
+   * @return string       Object type class
+   */
+  public static function get($type)
+  {
+    return is_string($type) && isset(static::$manufacturable[$type]) ? static::$manufacturable[$type] : null;
+  }
 
-    /**
-     * Checks if there is a manufacturable with target key
-     * 
-     * @param  string  $key Target key
-     * @return boolean      True if key exists, false otherwise
-     */
-    public static function canManufacture($key)
-    {
-        return is_string($key) && isset(static::$manufacturable[$key]) ? true : false;
+  /**
+   * Removes a new manufacturable class
+   * 
+   * @param string $type  Object type ID
+   */
+  public static function remove($type)
+  {
+    if (isset(static::$manufacturable[$type])) unset(static::$manufacturable[$type]);
+  }
+
+  /**
+   * Checks if there is a manufacturable with target key
+   * 
+   * @param  string  $key Target key
+   * @return boolean      True if key exists, false otherwise
+   */
+  public static function canManufacture($key)
+  {
+    return is_string($key) && isset(static::$manufacturable[$key]) ? true : false;
+  }
+
+  /**
+   * Returns the id to manufacture another instance of the passed object, if any
+   * 
+   * @param  object $instance Arg instance
+   * @return string           Arg ID 
+   */
+  public static function getInstanceId($instance)
+  {
+    if (is_object($instance)) {
+
+      $class = get_class($instance);
+      $id    = array_search($class, static::$manufacturable);
+
+      return $id ?: null;
     }
 
-    /**
-     * Returns the id to manufacture another instance of the passed object, if any
-     * 
-     * @param  object $instance Arg instance
-     * @return string           Arg ID 
-     */
-    public static function getInstanceId($instance)
-    {
-        if (is_object($instance)) {
+    return null;
+  }
 
-            $class = get_class($instance);
-            $id    = array_search($class, static::$manufacturable);
+  /**
+   * Creates instance of target class
+   * 
+   * @param  string $type Class ID
+   * @param  array  $args Class arguments
+   * @return object       Class instance
+   */
+  public static function create($type, array $args = array())
+  {
+    // Check if target is in the allowed list
+    if (array_key_exists($type, static::$manufacturable)) {
 
-            return $id ?: null;
-        }
+      $class_name = static::$manufacturable[$type];
 
-        return null;
+      // Get an instance of the target class
+      $obj = call_user_func_array([new \ReflectionClass($class_name), 'newInstance'], $args);
+
+      // Return object
+      return $obj;
     }
 
-	/**
-	 * Creates instance of target class
-	 * 
-	 * @param  string $type Class ID
-	 * @param  array  $args Class arguments
-	 * @return object       Class instance
-	 */
-	public static function create($type, array $args = array())
-	{
-		// Check if target is in the allowed list
-		if (array_key_exists($type, static::$manufacturable)) {
-
-			$class_name = static::$manufacturable[$type];
-
-			// Get an instance of the target class
-	        $obj = call_user_func_array(
-
-	        	array(
-					new \ReflectionClass($class_name), 
-					'newInstance'
-				), 
-	            $args
-	        );
-
-	    	// Return object
-	    	return $obj;
-		}
-
-		// Return null if target object is not manufacturable
-		return null;
-	}
+    // Return null if target object is not manufacturable
+    return null;
+  }
 }
