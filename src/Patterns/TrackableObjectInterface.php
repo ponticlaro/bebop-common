663eb132dafc105b9af839c686180e6dbfd5ac05
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-01-27 09:54:36 +0100

    added docblocks to TrackableObjectInterface methods

--- src/Patterns/TrackableObjectInterface.php
+++ src/Patterns/TrackableObjectInterface.php
@@ -4,6 +4,17 @@ namespace Ponticlaro\Bebop\Common\Patterns;
 
 interface TrackableObjectInterface {
 
+  /**
+   * Returns trackable object ID
+   * 
+   * @return string
+   */
 	public function getObjectID();
+
+  /**
+   * Returns trackable object type
+   * 
+   * @return string
+   */
 	public function getObjectType();
 }
