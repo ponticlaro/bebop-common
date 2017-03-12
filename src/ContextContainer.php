d7ee2aa12dce68da0b217359862c6944564e1254
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-03-12 11:26:27 +0100

    Docs: applied PSR-5 to Context classes

--- src/ContextContainer.php
+++ src/ContextContainer.php
@@ -1,32 +1,51 @@
 <?php
+/**
+ * Context Container class.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ */
 
 namespace Ponticlaro\Bebop\Common;
 
+/**
+ * Callable wrapper used to determine the current context from a \WP_Query instance.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ * @internal
+ */
 class ContextContainer {
 
   /**
-   * ID of this Context Container
-   * 
+   * Context Container ID
+   *
+   * @since 1.0.0
+   *
    * @var string
    */
   private $id;
 
   /**
-   * Function of this Context Container
-   * 
-   * @var string
+   * Context Container function
+   *
+   * @since 1.0.0
+   *
+   * @var callable
    */
   private $function;
 
   /**
    * Instantiates a Context Container
-   * 
-   * @param string $id       ID
+   *
+   * @since 1.0.0
+   *
+   * @param string $id ID
    * @param string $function Function to execute
    */
-  public function __construct($id, callable $function)
+  public function __construct( $id, callable $function )
   {
-    if (!is_string($id))
+    if ( ! is_string( $id ) )
       throw new \Exception('$id must be a string');
     
     $this->id       = $id;
@@ -35,7 +54,9 @@ class ContextContainer {
 
   /**
    * Returns ID
-   * 
+   *
+   * @since 1.0.0
+   *
    * @return string
    */
   public function getId()
@@ -45,7 +66,9 @@ class ContextContainer {
 
   /**
    * Returns function
-   * 
+   *
+   * @since 1.0.0
+   *
    * @return string
    */
   public function getFunction()
@@ -56,13 +79,13 @@ class ContextContainer {
   /**
    * Executes Context Container function,
    * passing $wp_query as the first argument
-   *  
+   *
+   * @since 1.0.0
+   *
    * @return string The current context key or null, if none was found
    */
-  public function run()
+  public function run( \WP_Query $wp_query )
   {
-    global $wp_query;
-
-    return call_user_func_array($this->function, array($wp_query));
+    return call_user_func_array( $this->function, [ $wp_query ] );
   }
 }
--- src/ContextManager.php
+++ src/ContextManager.php
@@ -1,91 +1,108 @@
 <?php
+/**
+ * Context Manager class.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ */
 
 namespace Ponticlaro\Bebop\Common;
 
-use Ponticlaro\Bebop\Common\ContextContainer;
-
 /**
- * This class must be instantiated via getInstance() before the 'wp' action hook,
- * otherwise it won't be able to get the current context
+ * Determines the current context from the global \WP_Query instance.
  *
- * Consequently all context containers must also be added before the 'wp' action hook,
- * otherwise those won't be used to find the correct context
- * 
+ * This class must be instantiated via getInstance() before the 'wp' action hook,
+ * otherwise it won't be able to get the current context.
+ *
+ * Consequently all Context Containers must also be added before the 'wp' action hook,
+ * otherwise those won't be used to find the correct context.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ * @api
  */
 class ContextManager {
 
 	/**
-	 * Class instance
+	 * Class instance.
 	 * 
+   * @since 1.0.0
+   *
 	 * @var object
 	 */
 	private static $instance;
 
   /**
-   * Current context key
+   * Current context key.
    * 
+   * @since 1.0.0
+   *
    * @var string
    */
   protected $current;
 
   /**
-   * Used to store the current context when
-   * manually defining the context key
+   * Used to store the current context when manually defining the context key.
    * 
+   * @since 1.0.0
+   *
    * @var array
    */
   protected $current_backups = [];
 
   /**
-   * List of Context Containers
+   * List of Context Containers.
    *  
+   * @since 1.0.0
+   *
    * @var array
    */
   protected $contexts = [];
 
   /**
-   * Instantiates Context Manager
+   * Instantiates Context Manager.
    * 
+   * @since 1.0.0
    */
   public function __construct()
   {
     // Add default context rules
-    $this->add('default', function($q) {
+    $this->add( 'default', function( $q ) {
 
       $key = null;
 
-      if ($q->is_home()) { $key = 'home/posts'; }
+      if ( $q->is_home() ) { $key = 'home/posts'; }
 
-      elseif ($q->is_front_page()) { $key = 'home/page'; }
+      elseif ( $q->is_front_page() ) { $key = 'home/page'; }
 
-      elseif ($q->is_search()) { $key = 'search'; }
+      elseif ( $q->is_search() ) { $key = 'search'; }
 
-      elseif ($q->is_404()) { $key = 'error/404'; }
+      elseif ( $q->is_404() ) { $key = 'error/404'; }
 
-      elseif ($q->is_category()) { $key = 'tax/category'; }
+      elseif ( $q->is_category() ) { $key = 'tax/category'; }
 
-      elseif ($q->is_tag()) { $key = 'tax/tag'; }
+      elseif ( $q->is_tag() ) { $key = 'tax/tag'; }
 
-      elseif ($q->is_tax()) { $key = 'tax/'. $q->get('taxonomy'); }
+      elseif ( $q->is_tax() ) { $key = 'tax/'. $q->get('taxonomy'); }
 
-      elseif ($q->is_post_type_archive()) { $key = 'archive/'. $q->get('post_type'); }
+      elseif ( $q->is_post_type_archive() ) { $key = 'archive/'. $q->get('post_type'); }
       
-      elseif ($q->is_date()) {
+      elseif ( $q->is_date() ) {
           
-        if ($q->is_year()) { $key = 'archive/date/year'; }
+        if ( $q->is_year() ) { $key = 'archive/date/year'; }
 
-        elseif ($q->is_month()) { $key = 'archive/date/month'; }
+        elseif ( $q->is_month() ) { $key = 'archive/date/month'; }
 
-        elseif ($q->is_day()) { $key = 'archive/date/day'; }
+        elseif ( $q->is_day() ) { $key = 'archive/date/day'; }
       }
 
-      elseif ($q->is_author()) { $key = 'archive/author'; }
+      elseif ( $q->is_author() ) { $key = 'archive/author'; }
 
-      elseif ($q->is_singular()) { 
+      elseif ( $q->is_singular() ) { 
 
-        if ($q->is_page()) { $post_type = 'page'; }
+        if ( $q->is_page() ) { $post_type = 'page'; }
 
-        elseif($q->get('post_type')) { $post_type = $q->get('post_type'); }
+        elseif( $q->get('post_type') ) { $post_type = $q->get('post_type'); }
 
         else { $post_type = 'post'; }
 
@@ -96,41 +113,49 @@ class ContextManager {
     });
     
     // Add action to define current context
-    add_action('wp', array($this, 'defineCurrent'));
+    add_action( 'wp', [ $this, 'defineCurrent' ]);
   }
 
   /**
-	 * Do not allow clones
+	 * Do not allow clones.
 	 * 
+   * @since 1.0.0
+   *
 	 * @return void
 	 */
   private final function __clone() {}
 
 	/**
-	 * Gets single instance of called class
+	 * Gets Context Manager single instance.
 	 * 
-	 * @return object
+   * @since 1.0.0
+   *
+	 * @return ContextManager Context Manager instance
 	 */
 	public static function getInstance() 
 	{
-		if (!isset(static::$instance))
+		if ( ! isset( static::$instance ) )
       static::$instance = new static();
 
     return static::$instance;
 	}
 
   /**
-   * Defines current contexts by running all
-   * context containers until it finds a match
+   * Defines current contexts by running all context containers until it finds a match.
    * 
+   * @since 1.0.0
+   * 
+   * @return ContextManager Context Manager instance
    */
   public function defineCurrent()
   {
-    foreach ($this->contexts as $context_container) {
-        
-      $key = $context_container->run();
+    global $wp_query;
 
-      if ($key) {
+    foreach ( $this->contexts as $context_container ) {
+        
+      $key = $context_container->run( $wp_query );
+
+      if ( $key ) {
 
         $this->current = $key;
         break;
@@ -141,34 +166,41 @@ class ContextManager {
   }
 
   /**
-   * Checks if the target $key is a partial match for the current context
-   * You can optionally pass a regular expression to find matches
+   * Checks if the target $key is a partial match for the current context.
    * 
-   * @param  string  $key        Context key or regular expression
-   * @param  boolean $is_pattern True if the $key should be treated as a regular expression
-   * @return boolean             True if there is a match, false otherwise
+   * You can optionally pass a regular expression to find matches.
+   * 
+   * @since 1.0.0
+   *
+   * @param string $key Context key or regular expression
+   * @param boolean $is_pattern True if the $key should be treated as a regular expression
+   * @return boolean True if there is a match, false otherwise
    */
-  public function is($key, $is_pattern = false)
+  public function is( $key, $is_pattern = false )
   {
-    $pattern = $is_pattern ? $key : '/^'. preg_quote($key, '/') .'*/';
+    $pattern = $is_pattern ? $key : '/^'. preg_quote( $key, '/' ) .'*/';
 
-    return preg_match($pattern, $this->current) ? true : false; 
+    return preg_match( $pattern, $this->current ) ? true : false; 
   }
 
   /**
-   * Checks if the target $key is an exact match for the current context
+   * Checks if the target $key is an exact match for the current context.
    * 
-   * @param  string  $key Context string to check
-   * @return boolean      True if it partially or fully matches, false otherwhise
+   * @since 1.0.0
+   *
+   * @param string $key Context string to check
+   * @return boolean True if it partially or fully matches, false otherwhise
    */
-  public function equals($key)
+  public function equals( $key )
   {
     return $this->current === $key ? true : false;
   }
 
   /**
-   * Returns current context key
+   * Returns current context key.
    * 
+   * @since 1.0.0
+   *
    * @return string
    */
   public function getCurrent()
@@ -177,16 +209,19 @@ class ContextManager {
   }
 
   /**
-   * Overrides current context with target $key,
-   * but also keeps a backup of the current key to later be restored
+   * Overrides current context.
+   *
+   * Keeps a backup of the current key to later be restored.
+   * Multiple overrides are allowed and tracked.
    * 
+   * @since 1.0.0
+   *
    * @param  string $key
-   * @return Ponticlaro\Bebop\Common\ContextManager Context Manager instance
+   * @return ContextManager Context Manager instance
    */
   public function overrideCurrent($key)
   {
-    if (is_string($key)) {
-        
+    if ( is_string( $key ) ) {
       $this->current_backups[] = $this->current;
       $this->current           = $key;
     }
@@ -195,85 +230,102 @@ class ContextManager {
   }
 
   /**
-   * Restores previois context key, if there is any
+   * Restores previous context key, if there is any.
    * 
-   * @return Ponticlaro\Bebop\Common\ContextManager Context Manager instance
+   * Multiple restores are allowed and tracked.
+   * 
+   * @since 1.0.0
+   *
+   * @return ContextManager Context Manager instance
    */
   public function restoreCurrent()
   {
-    if ($this->current_backups)
-      $this->current = array_pop($this->current_backups);
+    if ( $this->current_backups )
+      $this->current = array_pop( $this->current_backups );
 
     return $this;
   }
 
   /**
-   * Returns single context container by ID
+   * Returns single context container by ID.
    * 
-   * @param  string                                   $id ID of the target context container
-   * @return Ponticlaro\Bebop\Common\ContextContainer     Target context container
+   * @since 1.0.0
+   * @deprecated 1.1.7
+   *
+   * @param string $id ID of the target context container
+   * @return ContextContainer Target context container
    */
-  public function get($id)
+  public function get( $id )
   {
-    return $this->__getContextById($id);
+    return $this->__getContextById( $id );
   }
 
   /**
-   * Adds a single context container to the top of the list
+   * Adds a single context container to the top of the list.
    * 
+   * @since 1.0.0
+   *
    * @param string $id Context Container ID
-   * @param string $fn Context Container function
+   * @param callable $fn Context Container function
    */
-  public function add($id, $fn)
+  public function add( $id, callable $fn )
   {
-    $this->prepend($id, $fn);
+    $this->prepend( $id, $fn );
 
     return $this;
   }
 
   /**
-   * Adds a single context container to the top of the list
+   * Adds a single context container to the top of the list.
    * 
-   * @param  string                                 $id Context Container ID
-   * @param  callable                               $fn Context Container function
-   * @return Ponticlaro\Bebop\Common\ContextManager     Context Manager instance
+   * @since 1.0.0
+   * @uses Ponticlaro\Bebop\Common\ContextContainer
+   *
+   * @param string $id Context Container ID
+   * @param callable $fn Context Container function
+   * @return ContextManager Context Manager instance
    */
-  public function prepend($id, callable $fn)
+  public function prepend( $id, callable $fn )
   {
-    if (is_string($id))
-      array_unshift($this->contexts, new ContextContainer($id, $fn));
+    if ( is_string( $id ) )
+      array_unshift( $this->contexts, new ContextContainer( $id, $fn ) );
 
     return $this;
   }
 
   /**
-   * Adds a single context container to the bottom of the list
+   * Adds a single context container to the bottom of the list.
    * 
-   * @param  string                                 $id Context Container ID
-   * @param  callable                               $fn Context Container function
-   * @return Ponticlaro\Bebop\Common\ContextManager     Context Manager instance
+   * @since 1.0.0
+   * @uses ContextContainer
+   *
+   * @param string $id Context Container ID
+   * @param callable $fn Context Container function
+   * @return ContextManager Context Manager instance
    */
-  public function append($id, callable $fn)
+  public function append( $id, callable $fn )
   {   
-    if (is_string($id))
-      $this->contexts[] = new ContextContainer($id, $fn);
+    if ( is_string( $id ) )
+      $this->contexts[] = new ContextContainer( $id, $fn );
 
     return $this;
   }
 
   /**
-   * Returns a single context container by ID
+   * Returns a single context container by ID.
    * 
-   * @param  string                                   $id ID of the target context container
-   * @return Ponticlaro\Bebop\Common\ContextContainer     Target context container
+   * @since 1.0.0
+   *
+   * @param string $id ID of the target context container
+   * @return ContextContainer Target context container
    */
-  private function __getContextById($id)
+  private function __getContextById( $id )
   {
     $target_context = null;
 
-    foreach ($this->contexts as $context) {
+    foreach ( $this->contexts as $context ) {
         
-      if ($context->getId() == $id) {
+      if ( $context->getId() == $id ) {
           
         $target_context = $context;
         break;
