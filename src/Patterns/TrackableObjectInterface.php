73e80b2dba387d4de2979b4dd488f2e883f4f931
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-03-12 14:24:23 +0100

    Docs: applied PSR-5 to TrackableObjectInterface

--- src/Patterns/TrackableObjectInterface.php
+++ src/Patterns/TrackableObjectInterface.php
@@ -1,19 +1,36 @@
 <?php
+/**
+ * TrackableObject interface.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ */
 
 namespace Ponticlaro\Bebop\Common\Patterns;
 
+/**
+ * Interface for trackable objects.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ * @internal
+ */
 interface TrackableObjectInterface {
 
   /**
-   * Returns trackable object ID
+   * Returns trackable object ID.
    * 
+   * @since 1.0.0
+   *
    * @return string
    */
 	public function getObjectID();
 
   /**
-   * Returns trackable object type
+   * Returns trackable object type.
    * 
+   * @since 1.0.0
+   *
    * @return string
    */
 	public function getObjectType();
