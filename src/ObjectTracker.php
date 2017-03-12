a47adfb8dce7700844a0ffb7335f71790e29e12f
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-03-12 13:50:28 +0100

    Docs: applied PSR-5 to ObjectTracker

--- src/ObjectTracker.php
+++ src/ObjectTracker.php
@@ -1,20 +1,32 @@
 <?php
+/**
+ * ObjectTracker class.
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ */
 
 namespace Ponticlaro\Bebop\Common;
 
 use Ponticlaro\Bebop\Common\Patterns\TrackableObjectInterface;
+use Ponticlaro\Bebop\Common\Patterns\Traits\Singleton;
 
+/**
+ * Object tracker
+ *
+ * @package Bebop\Common
+ * @since 1.0.0
+ * @internal
+ */
 class ObjectTracker {
 
-	/**
-	 * Class instance
-	 * 
-	 * @var object
-	 */
-	private static $instance;
+	use Singleton;
 
 	/**
-	 * List with all the tracked object types lists
+	 * List with all the tracked object types lists.
+	 *
+   * @since 1.0.0
+   *
 	 * @var array
 	 */
 	protected $lists = [];
@@ -26,67 +38,51 @@ class ObjectTracker {
 	 */
   public function __construct() {}
 
-  /**
-	 * Do not allow clones
-	 * 
-	 * @return void
-	 */
-  private final function __clone() {}
-
 	/**
-	 * Gets single instance of called class
+	 * Tracks trackable objects.
 	 * 
-	 * @return object
+   * @since 1.0.0
+   *
+	 * @param  Ponticlaro\Bebop\Common\Patterns\TrackableObjectInterface $object Object to be tracked
+	 * @return ObjectTracker This class instance
 	 */
-	public static function getInstance() 
-	{
-		if (!isset(static::$instance))
-      static::$instance = new static();
-
-    return static::$instance;
-	}
-
-	/**
-	 * Tracks Bebop objects
-	 * 
-	 * @param  mixed $object Object to be tracked
-	 * @return void
-	 */
-	public function track(TrackableObjectInterface $object)
+	public function track( TrackableObjectInterface $object )
 	{	
 		// Get object details
 		$id   = (string) $object->getObjectID();
 		$type = (string) $object->getObjectType();
 
 		// Add object type collection if not already present
-		if ($type && !isset($this->lists[$type])) 
-			$this->lists[$type] = [];
+		if ( $type && ! isset( $this->lists[$type] ) ) 
+			$this->lists[ $type ] = [];
 
 		// Add object to its type collection
-		if($type && $id) 
-			$this->lists[$type][$id] = $object;
+		if( $type && $id ) 
+			$this->lists[ $type ][ $id ] = $object;
 
 		return $this;
 	}
 
 	/**
-	 * Returns a single object by "type" and "id"
+	 * Returns a single object by "type" and "id".
 	 * 
-	 * @param  string $type Type of the target object
-	 * @param  string $id   ID of the target object
-	 * @return object       Object that was stored in memory
+   * @since 1.0.0
+   *
+	 * @param string $type Type of the target object
+	 * @param string $id ID of the target object
+	 * @return object Object that was stored in memory
 	 */
-	public function get($type, $id)
+	public function get( $type, $id )
 	{
-		if (!is_string($type) || !is_string($id))
-			throw new \Exception("\$type and \$id arguments must be both strings");
+		if ( ! is_string( $type ) || ! is_string( $id ) )
+			throw new \Exception( "\$type and \$id arguments must be both strings" );
 
-		if (!isset($this->lists[$type]))
+		if ( ! isset( $this->lists[ $type ] ) )
 			return null;
 
-		if (!isset($this->lists[$type][$id]))
+		if ( ! isset( $this->lists[ $type ][ $id ] ) )
 			return null;
 
-		return $this->lists[$type][$id];
+		return $this->lists[ $type ][ $id ];
 	}
 }
